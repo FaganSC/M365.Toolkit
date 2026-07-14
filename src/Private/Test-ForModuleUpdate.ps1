2e3c5fc697c43c8688482abf69aa95da49ccf194
Shawn Fagan <FaganSC@gmail.com>
2026-07-14 15:31:30 -0400

    fix: update version check logic to use PowerShell Gallery API for latest version

--- src/Private/Test-ForModuleUpdate.ps1
+++ src/Private/Test-ForModuleUpdate.ps1
@@ -2,20 +2,27 @@ function Test-ForModuleUpdate {
     [cmdletbinding()]            
     param()
     $ModuleName = "M365.Toolkit"
-
-    $checkedVersion = False
-
+    $checkedVersion = (Get-Variable -Name M365ToolkitCheckedVersion -Scope Global -ErrorAction SilentlyContinue).Value
+    Write-Verbose "Checked Version: $checkedVersion"
     if ($checkedVersion -ne "Yes") {
         $currentVersion = ((Get-Module -ListAvailable | Where-Object { $_.Name -eq $ModuleName })[0] | Select-Object Version).Version.ToString()
-        $latestVersion = ((Invoke-WebRequest "https://api.github.com/repos/FaganSC/$($ModuleName)/releases" | ConvertFrom-Json)[0].tag_name -replace '^v', '')
+        Write-Verbose "Current Version: $currentVersion"
+        # PowerShell TestGallery API endpoint for the module
+        $Url = "https://www.powershellgallery.com/api/v2/FindPackagesById()?id='$ModuleName'"
 
-        if ($currentVersion -gt $latestVersion) {
+        $response = Invoke-RestMethod -Uri $Url -UseBasicParsing
+        $latestGalleryVersion = ($response | Sort-Object updated -Descending | Select-Object -First 1).properties.Version
+        Write-Verbose "Latest Gallery Version: $latestGalleryVersion"
+        if ($currentVersion -eq $latestGalleryVersion) {
+            Write-Verbose "You are using the latest version: $($currentVersion.ToString())"
+        }
+        if ($currentVersion -gt $latestGalleryVersion) {
             Write-Verbose "You are using an pre-release: $($currentVersion.ToString())"
             Write-Host -BackgroundColor Blue -ForegroundColor White "You are using a pre-release version of $($ModuleName)."
             Write-Host -BackgroundColor Blue -ForegroundColor White "Please check the PowerShell Gallery for the latest stable release."
         }
-        elseif ($currentVersion -ne $latestVersion) {
-            Write-Verbose "There is a new version available: $($latestVersion.ToString())"
+        elseif ($currentVersion -ne $latestGalleryVersion) {
+            Write-Verbose "There is a new version available: $($latestGalleryVersion.ToString())"
             Write-Host -BackgroundColor Yellow -ForegroundColor Black "There is a new version of $($ModuleName) available."
             Write-Host -BackgroundColor Yellow -ForegroundColor Black "Please update to the latest version."
         }
