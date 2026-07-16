8c5f15bcdc041c64637b946b8a99fff09e4d4b43
Shawn Fagan <FaganSC@gmail.com>
2026-07-15 22:59:34 -0400

    feat: enhance Clear-SP365List function with recycling option and improved user prompts

--- src/Public/SharePoint/Clear-SP365List.ps1
+++ src/Public/SharePoint/Clear-SP365List.ps1
@@ -2,7 +2,9 @@ function Clear-SP365List {
     [CmdletBinding()]
     param (
         [Parameter(Mandatory = $true)]
-        $ListName
+        $ListName,
+        [Parameter(Mandatory = $false)]
+        [switch]$Recycle = $false
     )
     try {
 
@@ -18,16 +20,44 @@ function Clear-SP365List {
 
         Test-ForModuleUpdate
 
+        $list = Get-PnPList -Identity $listName
+        if (-not $list) {
+            Write-Error "List '$listName' not found."
+            return
+        }
+        if ($Recycle) {
+            Write-Host -ForegroundColor Yellow "WARNING: This will Recycle all $($list.ItemCount) items in the list '$listName'."
+        }
+        else {
+            Write-Host -ForegroundColor Yellow "WARNING: This will permanently delete all $($list.ItemCount) items in the list '$listName'."
+        }
+        $input = Read-Host "Are you sure you want to continue? Type 'Y' to confirm"
+        if ($input -ne "Y") {
+            Write-Host -ForegroundColor Yellow "Operation cancelled."
+            return
+        }
+
         $deletedCounter = 0
         $itemTotal = (Get-PnPList -Identity $listName).ItemCount
         do {
-            Get-PnPListItem -List $listName -PageSize 4500 | ForEach-Object {
+            Write-Host -NoNewline "Retrieving items from list '$listName' (Total Items: $itemTotal)"
+            $items = Get-PnPListItem -List $listName -PageSize 1000 
+            Write-Host -ForegroundColor Green " .....Done!"
+            Write-Host -NoNewline "Processing items for $($Recycle ? 'Recycling' : 'Deleting')"
+            
+            $items | ForEach-Object {
                 $percent = "{0:n1}" -f (($deletedCounter / $itemTotal) * 100)
-                Write-Progress -Activity "Truncating SharePoint List: $listName (Items: $itemTotal)" -Status "$percent% Complete:" -PercentComplete $percent
-                Remove-PnPListItem -List $listName -Identity $_.Id -Force
+                Write-Progress -Activity "$($Recycle ? 'Recycling' : 'Deleting') SharePoint List Items: $listName (Items: $itemTotal)" -Status "$percent% Complete:" -PercentComplete $percent
+                if ($Recycle) {
+                    Remove-PnPListItem -List $listName -Identity $_.Id -Recycle -Force | Out-Null
+                }
+                else {
+                    Remove-PnPListItem -List $listName -Identity $_.Id -Force | Out-Null
+                }
                 $deletedCounter++
             }
         } until ((Get-PnPList -Identity $listName).ItemCount -eq 0)
+        Write-Host -ForegroundColor Green " .....Done!"
     }
     catch {
         if ($_.Exception.Message -eq "The current connection holds no SharePoint context. Please use one of the Connect-PnPOnline commands which uses the -Url argument to connect.") {
